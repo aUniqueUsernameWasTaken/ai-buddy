316dff8ceb7aabe265d7f59ec7b744112165d759
AUniqueUserName <auniqueusernamewastaken@proton.me>
2024-07-19 21:09:45 +0300

    added 'memory'

--- main.py
+++ main.py
@@ -7,16 +7,27 @@ import os
 
 count = 0
 
+stuffaskedbefore = {
+    "question": "What are you?",
+    "answer": "Im Mistral AI!"
+}
+
 while True:
     if os.path.exists("audio.wav"):
         os.remove("audio.wav")
     if os.path.exists("audio_converted.wav"):
         os.remove("audio_converted.wav")
 
-    print("remember to check if you entered a mistral AI API key in mistral.py")
-    tts.texttospeach(mistral.aishit(voice_recognition.dostuff()), counter=count)
-    
+    text = voice_recognition.dostuff()
+    aiResponseText = mistral.aishit(text, stuffaskedbefore=stuffaskedbefore)
+
+    try:
+        tts.texttospeach(aiResponseText, counter=count)
+    except TypeError:
+        print("remember to check if you entered a mistral AI API key in mistral.py")   
+
+     
     audioplaymodule.play("outputs/output"+str(count)+".mp3")
     
     count += 1
-
+    stuffaskedbefore.update({f"question{count}": text, f"answer{count}": aiResponseText})
--- mistral.py
+++ mistral.py
@@ -1,17 +1,17 @@
-def aishit(prompt):
+def aishit(prompt, stuffaskedbefore):
     import os
     from mistralai.client import MistralClient
     from mistralai.models.chat_completion import ChatMessage
 
     # Replace 'your_api_key' with your actual API key
-    api_key = "APIKEY"
+    api_key = "158fsto70jgSJhUn6FERNxH2zcOai9fk"
     model = "mistral-large-latest"
 
     client = MistralClient(api_key=api_key)
 
     chat_response = client.chat(
         model=model,
-        messages=[ChatMessage(role="user", content=prompt+"\n##########\nkeep your answer within 40 words MAX and try to aim for smaller messages please")]
+        messages=[ChatMessage(role="user", content="conversation\n#########\n"+str(stuffaskedbefore)+"\n############\nyour job is to respond appropriately according to the conversation\n###############"+prompt+"\n##########\nkeep your answer within 40 words MAX and try to aim for smaller messages please")]
     )
 
     print(chat_response.choices[0].message.content)
